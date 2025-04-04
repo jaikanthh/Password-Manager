3164207104d3dd81b9c733730b0652ebe21a4dbb
Jaikanth <2320090050@klh.edu.in>
2025-04-04 17:15:55 +0530

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,6 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || '',
   {
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.MYSQLPORT || 3306,
     dialect: 'mysql',
     logging: process.env.NODE_ENV === 'development',
     pool: {
